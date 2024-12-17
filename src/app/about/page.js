8d14c49234d5fdf0b79c450514efac3f9418146d
milesthomas <ths22115@towerhill.org>
2024-12-16 20:35:12 -0500

    made skeleton.js and moved index page.js contents to it, made navbar component, tweaked mark and tv-static animations

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+'use client'
+import React, { useRef, useEffect } from "react";
+import "./page.css";
+import Skeleton from './../components/skeleton';
+
+export default function About() {
+  return (
+    <Skeleton page={'about'} />
+  );
+}
