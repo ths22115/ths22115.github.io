21f8295e65545726bed5bc1bc94548184adb4ef3
milesthomas <ths22115@towerhill.org>
2024-12-31 12:46:04 -0600

    more progress on experience page, added job-title and job dates to hover and tweaked navbar filter tabs

--- src/app/components/job.js
+++ src/app/components/job.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 export default function Job(props) {
     /* .company == comapny name
@@ -9,11 +9,27 @@ export default function Job(props) {
        .end == end date 
        .cat == job category (swe, design, etc)
        .active == bool, true if position is still held */
-    console.log(props.active)
+    const [preview, setPreview] = useState(false);
+
+    // console.log(props.active)
+    function jobPreview(e) {
+        setPreview(true);
+    }
+    function jobDefault(e) {
+        setPreview(false);
+    }
+ 
     return (
-    <div className={"job " + (props.active ? "active-job" : "")}>
-        <div className="job-company">{props.company}</div>
-        <div className="job-title">{props.title}</div>
+    <div className={"job"} onMouseOver={jobPreview} onMouseLeave={jobDefault}>
+        <div className={"job-company"}>{props.company}</div>
+        <div className={"job-preview " + (preview ? "job-preview-active" : "") }>
+            <div className={"job-title"}>{props.title}</div> 
+            <div className="job-date">
+                ( <div className={"job-start"}>{props.start}</div> ~ 
+                <div className={"job-end"}>{props.end}</div> )
+            </div>
+        </div>
+        
     </div>
     )
 }
