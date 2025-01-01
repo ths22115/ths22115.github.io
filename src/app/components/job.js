f31d3f326989046080f18aa40ecbb0f8d9e192ab
milesthomas <ths22115@towerhill.org>
2024-12-31 23:17:54 -0600

    progress on job desc function

--- src/app/components/job.js
+++ src/app/components/job.js
@@ -10,17 +10,27 @@ export default function Job(props) {
        .cat == job category (swe, design, etc)
        .active == bool, true if position is still held */
     const [preview, setPreview] = useState(false);
+    const [focus, setFocus] = useState(false);
 
     // console.log(props.active)
-    function jobPreview(e) {
-        setPreview(true);
-    }
     function jobDefault(e) {
-        setPreview(false);
+        if (!focus) {
+            setPreview(false);
+        }
+        
+    }
+    function jobPreview(e) {
+        if( !focus) {
+            setPreview(true);
+        }
+    }
+    function jobFocus(e) {
+        setPreview(true);
+        setFocus(true);
     }
  
     return (
-    <div className={"job"} onMouseOver={jobPreview} onMouseLeave={jobDefault}>
+    <div id={props.id}className={"job"} onMouseOver={jobPreview} onMouseLeave={jobDefault} onClick={props.onClick}>
         <div className={"job-company"}>{props.company}</div>
         <div className={"job-preview " + (preview ? "job-preview-active" : "") }>
             <div className={"job-title"}>{props.title}</div> 
@@ -28,6 +38,19 @@ export default function Job(props) {
                 ( <div className={"job-start"}>{props.start}</div> ~ 
                 <div className={"job-end"}>{props.end}</div> )
             </div>
+        </div>
+        <div className={"job-desc " + (props.focus == props.id ? "job-desc-active" : "") }>
+        Designed, developed, and deployed a fully documented AWS Lambda API to deliver cloud maturity metrics for 
+        all 30+ cloud infrastructure systems used at Capital One, to be adopted by over 11,000 software engineers 
+        organized in over 2,000 agile teams.
+        <br />
+        Collaborated with associate and senior engineers in a scrum-based agile environment, leveraging JIRA for 
+        issue tracking and sprint management, Python for software implementation, GraphQL for complex database 
+        queries, and Jenkins for seamless deployment.
+        <br />
+        Utilized the Pytest framework to implement and execute over 50 unit tests designed with both black-box 
+        and white-box testing methodologies to validate API functionality and robustness, providing over 90% 
+        statement coverage comprehensively.
         </div>
         
     </div>
