2f3820d2a976f25ca776672fd93d35ebda36b341
milesthomas <ths22115@towerhill.org>
2025-01-08 17:28:05 -0500

    began work on port page, added imgs for design section and finished text formatting for web dev / ui,ux section

--- src/app/port/page.js
+++ src/app/port/page.js
@@ -1,13 +1,51 @@
 'use client'
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./page.css";
 import Skeleton from '../components/skeleton';
+import Project from '../components/project';
 
-export default function About() {
+export default function Portfolio() {
+  const [projectFocus, setProjectFocus] = useState(-1);
+  const [jobFilter, setJobFilter] = useState("all");
+  const [filterChange, setfilterChange] = useState(false);
+
+  function focusProject(e) {
+    // if(e.target.className)
+    var projectElem = e.target
+    // console.log(jobElem)
+    // console.log(jobElem.parentElement)
+    while(projectElem.className.split(" ")[0] !== "project") {
+      projectElem = projectElem.parentElement
+    }
+    setProjectFocus(projectFocus == projectElem.id ? -1 : projectElem.id)
+    // console.log(jobElem.props) 
+    // setJobFocus(jobElem.)
+    // console.log(e.target.className)
+    // setJobFocus(true);
+    // setFocus(true);
+  }
+
+  // function updateJobFilter(e) {
+  //   // console.log(e.target) 
+  //   setfilterChange(true)
+  //   setTimeout(function() { 
+  //     setJobFilter(e.target.id.split("-")[1])
+  //     setfilterChange(false)
+  //   }, 200);
+    
+  // }
+
   return (
     <div>
-    <Skeleton page={'port'} />
-    <div className="lol" style={{position: 'absolute', top: 50+'%', left: 50+'%', fontSize: 1+'rem'}}>LOL</div>
+    <Skeleton page={"port"}/>
+    <div className="port-container">
+      
+      <div className={"project-piece-list project-list"}>
+        <Project id={0} onClick={focusProject} focus={projectFocus}/>
+        <Project id={1} onClick={focusProject} focus={projectFocus}/>
+        <Project id={2} onClick={focusProject} focus={projectFocus}/>
+      </div>
+    </div>
     </div>
   );
 }
