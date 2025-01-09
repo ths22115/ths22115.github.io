46821b9763dd7a3b2a3c9a6605f6fcb8e4a0e641
milesthomas <ths22115@towerhill.org>
2025-01-08 19:13:59 -0500

    finished port page web dev section functionality

--- src/app/components/mark.js
+++ src/app/components/mark.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const Mark = React.forwardRef((props, ref) => {
     return (
-        <Image className={"mark  mark-" + props.page + (props.focus != -1 ? " mark-focus" : "")} src={MarkBlack} ref={ref} alt={"Mius Thomas Logo"} width={1000}/>
+        <Image className={"mark  mark-" + props.page + (props.focus > -1 ? " mark-focus" : "")} src={MarkBlack} ref={ref} alt={"Mius Thomas Logo"} width={1000}/>
     )
 });
 export default Mark;
--- src/app/port/page.js
+++ src/app/port/page.js
@@ -3,11 +3,14 @@ import React, { useRef, useEffect, useState } from "react";
 import "./page.css";
 import Skeleton from '../components/skeleton';
 import Project from '../components/project';
+import ProjectPreview from '../components/projectPreview';
 
 export default function Portfolio() {
-  const [projectFocus, setProjectFocus] = useState(-1);
-  const [jobFilter, setJobFilter] = useState("all");
-  const [filterChange, setfilterChange] = useState(false);
+  const [isProjectsActive, setIsProjectsActive] = useState(true);
+  const [portFocus, setPortFocus] = useState(-1);
+  const [portSection, setPortSection] = useState("webdev");
+  const [sectionChange, setSectionChange] = useState(false);
+  const [displayChange, setDisplayChange] = useState(false);
 
   function focusProject(e) {
     // if(e.target.className)
@@ -17,33 +20,43 @@ export default function Portfolio() {
     while(projectElem.className.split(" ")[0] !== "project") {
       projectElem = projectElem.parentElement
     }
-    setProjectFocus(projectFocus == projectElem.id ? -1 : projectElem.id)
-    // console.log(jobElem.props) 
-    // setJobFocus(jobElem.)
-    // console.log(e.target.className)
-    // setJobFocus(true);
-    // setFocus(true);
+
+    setDisplayChange(true)
+    setTimeout(function() { 
+      setPortFocus(portFocus == projectElem.id ? -1 : projectElem.id)
+      setDisplayChange(false)
+    }, 200);
   }
 
-  // function updateJobFilter(e) {
-  //   // console.log(e.target) 
-  //   setfilterChange(true)
-  //   setTimeout(function() { 
-  //     setJobFilter(e.target.id.split("-")[1])
-  //     setfilterChange(false)
-  //   }, 200);
-    
-  // }
-
+  function updatePortSection(e) {
+    // console.log(e.target) 
+    setSectionChange(true)
+    setTimeout(function() { 
+      setPortSection(e.target.id.split("-")[1])
+      setPortFocus(-1)
+      setSectionChange(false)
+    }, 200);
+  }
+ 
   return (
     <div>
-    <Skeleton page={"port"}/>
-    <div className="port-container">
-      
-      <div className={"project-piece-list project-list"}>
-        <Project id={0} onClick={focusProject} focus={projectFocus}/>
-        <Project id={1} onClick={focusProject} focus={projectFocus}/>
-        <Project id={2} onClick={focusProject} focus={projectFocus}/>
+    <Skeleton page={"port"} portFocus={portFocus} portSection={portSection} updatePortSection={updatePortSection}/>
+    <div className={"port-container " + (sectionChange ? "filter-trans" : "")}>
+      <div className={"project-display " + (portSection == "webdev" ? "display-active " : "") + (displayChange ? "filter-trans" : "")}>
+        <ProjectPreview src={"samothSite1.jpg"} project={0} focus={portFocus}/>
+        <ProjectPreview src={"samothSite1.jpg"} project={0} focus={portFocus}/>
+        <ProjectPreview src={"samothSite1.jpg"} project={0} focus={portFocus}/>
+        <ProjectPreview src={"samothSite2.jpg"} project={1} focus={portFocus}/>
+        <ProjectPreview src={"samothSite2.jpg"} project={1} focus={portFocus}/>
+        <ProjectPreview src={"samothSite2.jpg"} project={1} focus={portFocus}/>
+        <ProjectPreview src={"samothSite3.jpg"} project={2} focus={portFocus}/>
+        <ProjectPreview src={"samothSite3.jpg"} project={2} focus={portFocus}/>
+        <ProjectPreview src={"samothSite3.jpg"} project={2} focus={portFocus}/>
+      </div>
+      <div className={"project-piece-list project-list " + (portSection == "webdev" ? "list-active" : "")}>
+        <Project id={0} onClick={focusProject} focus={portFocus}/>
+        <Project id={1} onClick={focusProject} focus={portFocus}/>
+        <Project id={2} onClick={focusProject} focus={portFocus}/>
       </div>
     </div>
     </div>
