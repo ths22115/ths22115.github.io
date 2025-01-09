f8278da732163ff74df3f2340c6fb92f6c661b4e
milesthomas <ths22115@towerhill.org>
2025-01-09 10:24:17 -0500

    tweaked landing page styling

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,9 @@ import Skeleton from './components/skeleton';
 
 export default function Landing() {
   return (
+    <div>
     <Skeleton page={'landing'} />
+    <div className="landing-text">Hi. I create that of which emobides me.</div>
+    </div>
   );
 }
