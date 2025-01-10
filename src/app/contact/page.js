9a63fa3538aefeca4cd8abba6c80a5757c9eb979
milesthomas <ths22115@towerhill.org>
2025-01-10 05:02:09 -0500

    fix contact hyperlink issue

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -7,9 +7,9 @@ export default function About() {
   return (
     <div>
     <Skeleton page={'contact'} />
-    <div className="text contact-text">
-        <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"milesmckinleythomas@gmail.com"}>LINKEDIN<div className={"hyperlink-arrow"}>&#8599;</div></a></div>
-        <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"https://www.linkedin.com/in/miles-thomas-552648245/"}>EMAIL<div className={"hyperlink-arrow"}>&#8599;</div></a></div>
+    <div className="text contact-text"> 
+        <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"https://www.linkedin.com/in/miles-thomas-552648245/"}>LINKEDIN<div className={"hyperlink-arrow"}>&#8599;</div></a></div>
+        <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"milesmckinleythomas@gmail.com"}>EMAIL<div className={"hyperlink-arrow"}>&#8599;</div></a></div>
         <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"https://www.instagram.com/urm1us/"}>INSTAGRAM<div className={"hyperlink-arrow"}>&#8599;</div></a></div>
       </div>
     </div>
