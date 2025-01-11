2562cc930145be672c5364c70b59831e729f6710
milesthomas <ths22115@towerhill.org>
2025-01-11 16:36:23 -0500

    email hyperlink fix

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -9,7 +9,7 @@ export default function About() {
     <Skeleton page={'contact'} />
     <div className="text contact-text"> 
         <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"https://www.linkedin.com/in/miles-thomas-552648245/"}>LINKEDIN<div className={"hyperlink-arrow"}>&#8599;</div></a></div>
-        <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"milesmckinleythomas@gmail.com"}>EMAIL<div className={"hyperlink-arrow"}>&#8599;</div></a></div>
+        <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"mailto:milesmckinleythomas@gmail.com"}>EMAIL<div className={"hyperlink-arrow"}>&#8599;</div></a></div>
         <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"https://www.instagram.com/urm1us/"}>INSTAGRAM<div className={"hyperlink-arrow"}>&#8599;</div></a></div>
       </div>
     </div>
