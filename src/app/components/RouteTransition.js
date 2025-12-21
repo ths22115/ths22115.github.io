069bdb1d36f12398193a205b70b519899ab31b2a
milesthomas <ths22115@towerhill.org>
2025-12-21 02:36:09 -0500

    pretty happy w transition as is, breaks mobile view tho

--- src/app/components/RouteTransition.js
+++ src/app/components/RouteTransition.js
@@ -1,14 +1,12 @@
 'use client'
-
 import { useEffect, useState, useRef } from 'react'
 import { usePathname } from 'next/navigation'
-import './route-transition.css'
-// import './skeleton.css'
-import Mark from './mark'
+import './routeTransition.css'
+import Image from 'next/image'
 
 export default function RouteTransition({ children }) {
   const pathname = usePathname()
-  const [isTransitioning, setIsTransitioning] = useState(false)
+  const [isTransitioning, setIsTransitioning] = useState(false )
   const prevPathnameRef = useRef(pathname)
 
   useEffect(() => {
@@ -20,7 +18,7 @@ export default function RouteTransition({ children }) {
       // hide transition after 800ms
       const hideTimer = setTimeout(() => {
         setIsTransitioning(false)
-      }, 800)
+      }, 400)
 
       return () => {
         clearTimeout(hideTimer)
@@ -32,15 +30,17 @@ export default function RouteTransition({ children }) {
     <>
       {isTransitioning && (
           <div className={"route-transition-overlay"}>
-              {/* <div className="route-transition-content"> */}
-                  <div className="trans-noise-wrapper"></div>
-                  <div className={"trans-scanlines"}></div>
-                  <div className={"trans-noise"}></div>
+              <div className="trans-noise-wrapper"></div>
+              <div className={"trans-scanlines"}></div>
+              <div className={"trans-noise"}></div>
+              <Image src="/miusWordmarkBlack.png" alt="Mius Thomas Logo" width={900} height={923} className="trans-wordmark" />
+              <div className="route-transition-content">
+                  
                   {/* <div className={"noise"}></div> */}
                   {/* <Mark ref={markRef} page={props.page} size={markWidth} isMobile={props.isMobile} focus={(props.page == 'exp' ? props.expFocus : props.portFocus)}/> */}
                   {/* You can customize this content */}
                   {/* <div className="route-transition-spinner"></div> */}
-              {/* </div> */}
+              </div>
           </div>
       )}
       <div>
