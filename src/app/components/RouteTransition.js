028049c72921dc721eeed477620855b0c7c6821a
milesthomas <ths22115@towerhill.org>
2025-12-25 09:44:28 -0500

    mobile view bug fix, about page still broken

--- src/app/components/RouteTransition.js
+++ src/app/components/RouteTransition.js
@@ -43,9 +43,7 @@ export default function RouteTransition({ children }) {
               </div>
           </div>
       )}
-      <div>
-        {children}
-      </div>
+      {children}
     </>
   )
 }
