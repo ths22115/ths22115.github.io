b5840d0cec2428df6e11fd0314711fbfdbbe4ef8
milesthomas <ths22115@towerhill.org>
2026-06-27 10:37:26 -0400

    added glitch hovers to pieces

--- src/app/components/piece.js
+++ src/app/components/piece.js
@@ -2,9 +2,11 @@
 
 import Link from "next/link";
 import { formatPieceType } from "../lib/piece-format";
+import { useStaticEffects } from "../contexts/static-effects-context";
 
 export default function Piece(props) {
   const pieceHref = `/port/piece/${props.id}`;
+  const { isStaticEnabled } = useStaticEffects();
 
   if (props.isMobile) {
     return (
@@ -23,14 +25,23 @@ export default function Piece(props) {
     );
   }
 
+  const glitchDelay = `${((Number(props.id) || 0) % 5) * 0.2}s`;
+
   return (
-    <Link href={pieceHref}>
-      <img
-        id={props.id}
-        className="piece-img piece-gallery-img"
-        src={props.src}
-        alt={props.title}
-      />
-    </Link>
+    <div className={"piece-card " + (isStaticEnabled ? "piece-card--static" : "piece-card--bw")}>
+      <Link href={pieceHref} className="piece-link">
+        <span
+          className="piece-glitch"
+          style={{ "--piece-img": `url(${props.src})`, "--glitch-delay": glitchDelay }}
+        >
+          <img
+            id={props.id}
+            className="piece-img piece-gallery-img"
+            src={props.src}
+            alt={props.title}
+          />
+        </span>
+      </Link>
+    </div>
   );
 }
