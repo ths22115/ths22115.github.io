f2c62cadb51dbb329d5e0e7bebf9df08fc026ab0
milesthomas <ths22115@towerhill.org>
2026-06-27 09:22:17 -0400

    updated landing hero line, tweaked landing, app-shell styling

--- src/app/landing-view.js
+++ src/app/landing-view.js
@@ -1,14 +1,28 @@
 'use client'
 import React from "react";
+import "./landing.css";
 import { useMobile } from "./contexts/mobile-context";
 
 export default function LandingView() {
   const { isMobile } = useMobile();
+
   return (
     <div className="text landing-text">
-      { isMobile ? 
-      "Software engineer and designer creating that which embodies the self." :
-      "Creating that which embodies the self."
+      {
+        isMobile
+          ? "Engineer by logic.\nDesigner by instinct.\nBuilding products as beautiful as they are meaningful."
+          :
+          <>
+            <div className={"subtext landing-subtext"}>
+              Engineer by logic.
+            </div>
+            <div className={"subtext landing-subtext"}>
+              Designer by instinct.
+            </div>
+            <div className={"subtext landing-subtext"}>
+              Building products as beautiful as they are meaningful.
+            </div>
+          </>
       }
     </div>
   );
