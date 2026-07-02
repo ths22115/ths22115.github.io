b9693a124018651e6f87d226b1806e2226810b20
milesthomas <ths22115@towerhill.org>
2026-07-01 20:40:22 -0400

    tweaked about page text a little more

--- src/app/about/about-view.js
+++ src/app/about/about-view.js
@@ -9,14 +9,13 @@ const { isMobile } = useMobile();
   return (
     <div className="text about-text">
       <div className={"subtext about-subtext" + (isMobile ? " mobile" : "")}>
-        21-year-old Northern Virginia native based in Greater Baltimore.
+        21-year-old Northern Virginia native currently based in Greater Baltimore.
       </div>
       <div className={"subtext about-subtext" + (isMobile ? " mobile" : "")}>
         Seeking full-time opportunities at the intersection of software engineering and design.
       </div>
       <div className={"subtext about-subtext" + (isMobile ? " mobile" : "")}>
-        Received a B.S. in Computer Science with minors in 
-        Information Technology & Web Science and Graphic Design from Rensselaer Polytechnic Institute.
+        2026 graduate of Rensselaer Polytechnic Institute — B.S. in Computer Science, minors in IT & Web Science and Graphic Design.
       </div>
       <div className={"subtext about-subtext" + (isMobile ? " mobile" : "")}>
         Passionate designer, web developer, stylist, soccer player, creative, and Manchester United fan.
