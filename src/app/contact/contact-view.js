3c124ffacf1bf96ddad97c8c3d37470443705927
milesthomas <ths22115@towerhill.org>
2026-07-07 13:28:16 -0400

    updated contact email; tweaked mobile mark opacities

--- src/app/contact/contact-view.js
+++ src/app/contact/contact-view.js
@@ -5,7 +5,7 @@ import "./contact.css";
 export default function ContactView() {
   return (
     <div className="text contact-text">
-        <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"mailto:milesmckinleythomas@gmail.com"}>EMAIL<div className={"hyperlink-arrow"}>&#8599;&#xFE0E;</div></a></div>
+        <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"mailto:milesmmthomas@gmail.com"}>EMAIL<div className={"hyperlink-arrow"}>&#8599;&#xFE0E;</div></a></div>
         <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"https://www.linkedin.com/in/mius-thomas/"}>LINKEDIN<div className={"hyperlink-arrow"}>&#8599;&#xFE0E;</div></a></div>
         <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"https://www.github.com/ths22115"}>GITHUB<div className={"hyperlink-arrow"}>&#8599;&#xFE0E;</div></a></div>
         {/* <div className="subtext contact-subtext"><a className={"hyperlink"} target="_blank" href={"https://www.instagram.com/urm1us/"}>INSTAGRAM<div className={"hyperlink-arrow"}>&#8599;&#xFE0E;</div></a></div> */}
